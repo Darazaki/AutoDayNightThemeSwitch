830c5f26c938b0e9135ccde6bc2cff85c368de0e
Naqua Darazaki <n.darazaki@gmail.com>
2019-11-16 19:52:13 +0100

    First commit

---
+++ extension.js
@@ -0,0 +1,244 @@
+'use strict';
+
+
+// Imports:
+const { GLib, Gio, GObject } = imports.gi;
+const MainLoop = imports.mainloop;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
+
+/** Identifier used for the time checking loop */
+let timeCheckId = 0;
+/** 
+ * Optional bool that indicates if the night theme has been set
+ * 
+ * This variable can take the following values:
+ * 
+ * - `null` => unknown
+ * - `true` => night theme set
+ * - `false` => light theme set
+ * 
+ * If unknown, the theme corresponding to the current time will be set
+ * 
+ * @type {boolean | null}
+ */
+let hasNightThemeBeenSet = null;
+
+/** Extension's settings */
+let settings = null;
+
+
+// VALUES TAKEN FROM SETTINGS (BEGIN)
+// The following values do not have any impact on the program and are just there for
+// type checking purpose
+
+/** The period at which the time of the day is checked */
+let timeCheckPeriod = 1 /* ms */;
+
+/** Start and end of the nighttime */
+const nighttime = {
+    // Expressed in minutes since the start of the day
+
+    /** Night begins, day ends */
+    begin: 0 /* minutes */, 
+    /** Night ends, day begin */
+    end: 0 /* minutes */
+};
+
+/** Day and night themes */
+const theme = {
+    /** Day theme's name */
+    day: '',
+    /** Night theme's name */
+    night: ''
+}
+// VALUES TAKEN FROM SETTINGS (END)
+
+
+/** Get the values from the settings and make it so they can be updated automatically */
+function setupSettings() {
+    // Get the GSchema source so we can lookup our settings
+    let schema = Gio.SettingsSchemaSource.new_from_directory(
+        Me.dir.get_child('schemas').get_path(),
+        Gio.SettingsSchemaSource.get_default(),
+        false,
+    );
+
+    // Get the settings
+    settings = new Gio.Settings({
+        settings_schema: schema.lookup(
+            'org.gnome.shell.extensions.adnts@n.darazaki.gmail.com', true)
+    });
+
+    // Read settings
+    theme.day = settings.get_string('day-theme');
+    theme.night = settings.get_string('night-theme');
+    nighttime.begin = settings.get_uint('nighttime-begin');
+    nighttime.end = settings.get_uint('nighttime-end');
+    timeCheckPeriod = settings.get_uint('time-check-period');
+
+
+    // Watch for changes
+
+
+    // DAY THEME
+    settings.connect('changed::day-theme', function() {
+        theme.day = settings.get_string('day-theme');
+        if (hasNightThemeBeenSet === false) {
+            // It's daytime
+
+            // This will set the new theme after a certain amount of time,
+            // it was made to avoid the lag created by `setGTKTheme`
+            hasNightThemeBeenSet = null;
+
+            // Uncommenting this will make theme changes instantaneous but it
+            // will also make your computer lag when executing this
+
+            //setGTKTheme(theme.day);
+        }
+    });
+
+    // NIGHT THEME
+    settings.connect('changed::night-theme', function() {
+        theme.night = settings.get_string('night-theme');
+        if (hasNightThemeBeenSet === true) {
+            // It's nighttime
+
+            // This will set the new theme after a certain amount of time,
+            // it was made to avoid the lag created by `setGTKTheme`
+            hasNightThemeBeenSet = null;
+
+            // Uncommenting this will make theme changes instantaneous but it
+            // will also make your computer lag when executing this
+
+            //setGTKTheme(theme.night);
+        }
+    });
+
+    // NIGHT TIME BEGIN
+    settings.connect('changed::nighttime-begin', function() {
+        // This it automatically updated so no need for extra tweaks
+        nighttime.begin = settings.get_uint('nighttime-begin');
+    });
+    
+    // NIGHT TIME END
+    settings.connect('changed::nighttime-end', function() {
+        // This it automatically updated so no need for extra tweaks
+        nighttime.end = settings.get_uint('nighttime-end');
+    });
+
+    // TIME CHECK PERIOD
+    settings.connect('changed::time-check-period', function() {
+        timeCheckPeriod = settings.get_uint('time-check-period');
+        
+        // Replace the period
+        MainLoop.source_remove(timeCheckId);
+        timeCheckId = MainLoop.timeout_add(timeCheckPeriod, timeCheck, null);
+    });
+}
+
+
+/**
+ * Set the default GTK theme to a new theme.
+ * @param {string} themeName The new theme's name.
+ */
+function setGTKTheme(themeName) {
+    // Some 'Theme'\ -> Some \'Theme\'\\
+    themeName = themeName.replace(/[\\]/g, '\\\\').replace(/[\'']/g, '\\\'');
+
+    // Exec: gsettings set org.gnome.desktop.interface gtk-theme 'My Awesome Theme'
+    const cmdSetBase = 'gsettings set org.gnome.desktop.interface gtk-theme \'';
+    GLib.spawn_command_line_async(cmdSetBase + themeName + '\'');
+}
+
+
+/** Set the GTK theme to the night theme if not already done */
+function setNightThemeIfNeeded() {
+    if (hasNightThemeBeenSet !== true) {
+        // Either night theme or unknown
+
+        hasNightThemeBeenSet = true;
+
+        setGTKTheme(theme.night);
+    }
+}
+
+
+/** Set the GTK theme to the day theme if not already done */
+function setDayThemeIfNeeded() {
+    if (hasNightThemeBeenSet !== false) {
+        // Either day theme or unknown
+
+        hasNightThemeBeenSet = false;
+        
+        setGTKTheme(theme.day);
+    }
+}
+
+
+/**
+ * Check if it is day or night time and set the theme accordingly if needed
+ * @returns {boolean} true (which means repeat the loop indefinitely)
+ */
+function timeCheck() {
+    let now = new Date();
+    // Minutes elapsed since midnight
+    let minutesInDay = now.getHours() * 60 + now.getMinutes();
+    
+    if (nighttime.begin < nighttime.end) {
+        //   day    night    day
+        // +++++++---------+++++++
+
+        if (nighttime.begin <= minutesInDay && minutesInDay < nighttime.end) {
+            // Night
+
+            setNightThemeIfNeeded();
+        } else {
+            // Day
+
+            setDayThemeIfNeeded();
+        }
+    } else /* if (nightTime.begin >= nightTime.end) */ {
+        //  night (end)   day   night (begin)
+        // -------------+++++++---------------
+
+        if (nighttime.end <= minutesInDay && minutesInDay < nighttime.begin) {
+            // Day
+
+            setDayThemeIfNeeded();
+        } else {
+            // Night
+
+            setNightThemeIfNeeded();
+        }
+    }
+
+    return true /* repeat */;
+}
+
+
+/** Executed when the extension is enabled by the user or on session boot */
+function enable() {
+    // Initial setup for reading user preferences
+    setupSettings();
+
+    // Run now
+    timeCheck();
+    // Run every 20s starting from now
+    timeCheckId = MainLoop.timeout_add(timeCheckPeriod, timeCheck, null);
+}
+
+
+/** Executed when the extension is disabled by the user or on session shutdown */
+function disable() {
+    // Remove the repeating check
+    MainLoop.source_remove(timeCheckId);
+}
+
+
+/**
+ * This function is absolutely vital to the proper functioning of this extension,
+ * and what it does is literally nothing
+ */
+function init() {}
---
+++ prefs.js
@@ -0,0 +1,172 @@
+'use strict';
+
+
+// Imports:
+const { Gio, Gtk } = imports.gi;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
+
+/**
+ * This function is required to make this script work but doesn't do anything
+ */
+function init() { }
+
+
+/** Create the widget and bind its children to the actual values
+ *
+ *  @returns {Gtk.Grid} The grid containing all the widgets
+ */
+function buildPrefsWidget() {
+    const schema = Gio.SettingsSchemaSource.new_from_directory(
+        Me.dir.get_child('schemas').get_path(),
+        Gio.SettingsSchemaSource.get_default(),
+        false
+    );
+
+    let settings = new Gio.Settings({
+        settings_schema: schema.lookup(
+            'org.gnome.shell.extensions.adnts@n.darazaki.gmail.com', true)
+    });
+
+    let prefWidget = new Gtk.Grid({
+        margin: 18,
+        column_spacing: 12,
+        row_spacing: 12,
+        visible: true,
+        expand: true,
+    });
+
+    // THEMES HEADER
+
+    let titleThemes = new Gtk.Label({
+        label: '<b>Day/Night Themes</b>',
+        halign: Gtk.Align.START,
+        use_markup: true,
+        visible: true,
+    });
+    prefWidget.attach(titleThemes, 0, 0, 2, 1);
+
+    // THEME DAY
+
+    let labelThemeDay = new Gtk.Label({
+        label: 'Day Theme',
+        visible: true,
+    });
+    prefWidget.attach(labelThemeDay, 0, 1, 1, 1);
+
+    let entryThemeDay = new Gtk.Entry({
+        text: settings.get_string('day-theme'),
+        visible: true,
+        hexpand: true,
+    });
+    prefWidget.attach(entryThemeDay, 1, 1, 1, 1);
+    settings.bind(
+        'day-theme',
+        entryThemeDay,
+        'text',
+        Gio.SettingsBindFlags.DEFAULT,
+    );
+
+    // THEME NIGHT
+
+    let labelThemeNight = new Gtk.Label({
+        label: 'Night Theme',
+        visible: true,
+    });
+    prefWidget.attach(labelThemeNight, 0, 2, 1, 1);
+
+    let entryThemeNight = new Gtk.Entry({
+        text: settings.get_string('night-theme'),
+        visible: true,
+        hexpand: true,
+    });
+    prefWidget.attach(entryThemeNight, 1, 2, 1, 1);
+    settings.bind(
+        'night-theme',
+        entryThemeNight,
+        'text',
+        Gio.SettingsBindFlags.DEFAULT,
+    );
+
+    // RESET THEMES
+
+    let buttonResetThemes = new Gtk.Button({
+        label: 'Reset Themes',
+        visible: true,
+        hexpand: true,
+    });
+    prefWidget.attach(buttonResetThemes, 0, 3, 2, 1);
+    buttonResetThemes.connect('clicked', function() {
+        // reset themes
+    });
+
+    // NIGHTTIME HEADER
+
+    let titleNighttime = new Gtk.Label({
+        label: '<b>Nighttime (expressed in minutes since midnight)</b>',
+        halign: Gtk.Align.START,
+        use_markup: true,
+        visible: true,
+    });
+    prefWidget.attach(titleNighttime, 0, 4, 2, 1);
+
+    // NIGHTTIME BEGIN
+
+    let labelNighttimeBegin = new Gtk.Label({
+        label: 'Start of Nighttime',
+        visible: true,
+    });
+    prefWidget.attach(labelNighttimeBegin, 0, 5, 1, 1);
+
+    let spinNighttimeBegin = new Gtk.SpinButton({
+        climb_rate: 60.0,
+        value: settings.get_uint('night-time-begin'),
+        visible: true,
+        hexpand: true,
+    });
+    settings.bind(
+        'night-time-begin',
+        spinNighttimeBegin,
+        'value',
+        Gio.SettingsBindFlags.DEFAULT,
+    );
+    prefWidget.attach(spinNighttimeBegin, 1, 5, 1, 1);
+
+    // NIGHTTIME END
+
+    let labelNighttimeEnd = new Gtk.Label({
+        label: 'End of Nighttime',
+        visible: true,
+    });
+    prefWidget.attach(labelNighttimeEnd, 0, 6, 1, 1);
+
+    let spinNighttimeEnd = new Gtk.SpinButton({
+        climb_rate: 60.0,
+        value: settings.get_uint('night-time-end'),
+        visible: true,
+        hexpand: true,
+    });
+    settings.bind(
+        'night-time-end',
+        spinNighttimeEnd,
+        'value',
+        Gio.SettingsBindFlags.DEFAULT,
+    );
+    prefWidget.attach(spinNighttimeEnd, 1, 6, 1, 1);
+
+    // RESET NIGHTTIME
+
+    let buttonResetNighttime = new Gtk.Button({
+        label: 'Reset Nighttime',
+        visible: true,
+        hexpand: true,
+    });
+    prefWidget.attach(buttonResetNighttime, 0, 7, 2, 1);
+    buttonResetNighttime.connect('clicked', function() {
+        // reset nighttime
+    });
+
+
+    return prefWidget;
+}
