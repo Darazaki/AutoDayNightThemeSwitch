53a2a627649e33d1620cbb2f1913e22c9bf24dac
Naqua Darazaki <n.darazaki@gmail.com>
2020-05-08 21:50:29 +0200

    Make install.sh exit on failure and fix path variable

--- install.sh
+++ install.sh
@@ -6,20 +6,26 @@ src=$(dirname "$0")
 # Default the installation path to '~/.local/share/gnome-shell/extensions'
 if [ $# -ge 1 ]
 then
-    path=$@
+    path="$@"
 else
     path=~/.local/share/gnome-shell/extensions
 fi
+
+# Called if something goes wrong
+die() {
+    echo 'Installation failed:' "$@"
+    exit 1
+}
 
 # Check that the installation path is a directory
 if [ ! -e "$path" ]
 then
     # Create it if it doesn't exist yet
-    mkdir -p "$path"
+    mkdir -p "$path" \
+        || die "Cannot create '$path'"
 elif [ ! -d "$path" ]
 then
-    echo "'$path' is not a directory"
-    exit 1
+    die "'$path' is not a directory"
 fi
 
 # Remove previous installation if any
@@ -28,22 +34,28 @@ then
     read -p 'Previous installation found. Replace it? [Y/n]: ' choice
     if [ -z "$choice" ] || [ "$choice" = 'y' ] || [ "$choice" = 'Y' ]
     then
-        rm -rf "$path/adnts@n.darazaki"
+        rm -rf "$path/adnts@n.darazaki" \
+            || die "Cannot remove '$path/adnts@n.darazaki'"
     else
+        echo 'Nothing installed'
         exit 1
     fi
 fi
 
 # Create the extension directory
-mkdir -p "$path/adnts@n.darazaki/schemas"
+mkdir -p "$path/adnts@n.darazaki/schemas" \
+    || die "Cannot create directory '$path/adnts@n.darazaki/schemas'"
 
 # Copy the necessary files
-cp "$src/extension.js" "$path/adnts@n.darazaki"
-cp "$src/prefs.js" "$path/adnts@n.darazaki"
-cp "$src/metadata.json" "$path/adnts@n.darazaki"
-cp "$src/schemas/gschemas.compiled" "$path/adnts@n.darazaki/schemas"
-cp "$src/schemas/org.gnome.shell.extensions.adnts@n.darazaki.gschema.xml" \
-    "$path/adnts@n.darazaki/schemas"
+cp "$src/extension.js" \
+    "$src/prefs.js" \
+    "$src/metadata.json" \
+    "$path/adnts@n.darazaki" \
+    || die "Cannot copy files to '$path/adnts@n.darazaki'"
+cp "$src/schemas/gschemas.compiled" \
+    "$src/schemas/org.gnome.shell.extensions.adnts@n.darazaki.gschema.xml" \
+    "$path/adnts@n.darazaki/schemas" \
+    || die "Cannot copy files to '$path/adnts@n.darazaki/schemas'"
 
 # Done!
 echo 'Installation finished!'
