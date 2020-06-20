a66307a6de39153985a90dd3bab295a5ea18e08a
Naqua Darazaki <n.darazaki@gmail.com>
2020-06-20 19:38:29 +0200

    Fix `Global.extension` not being writable in `extension.js`

--- extension.js
+++ extension.js
@@ -3,23 +3,22 @@
 
 // Imports:
 const Me = imports.misc.extensionUtils.getCurrentExtension();
-const { extension } = Me.imports.modules.Global;
-const { Main } = Me.imports.modules;
+const { Global, Main } = Me.imports.modules;
 
 
 /** Called automatically when the extension is initialized */
 function init() {
-    extension = new Main.Module();
+    Global.extension = new Main.Module();
 }
 
 
 /** Called automatically when the extension is enabled */
 function enable() {
-    extension.enabled = true;
+    Global.extension.enabled = true;
 }
 
 
 /** Called automatically when the extension is disabled */
 function disable() {
-    extension.enabled = false;
+    Global.extension.enabled = false;
 }
